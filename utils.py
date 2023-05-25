32c8ea0461da4a38d981eeaa75e3e1c53cbc438e
burnaschev <informal201144@gmail.com>
2023-05-25 20:09:41 +0400

    added func removing empty json file

--- utils.py
+++ utils.py
@@ -1,9 +1,22 @@
 import json
 
 
+# import datetime
+
 def load_jsonfile(filename):
     """Считываем из файла данные и
     переводим на язык python"""
     with open(filename, encoding='utf8') as file:
         return json.loads(file.read())
+
+
+def removing_empty(filename):
+    new_list = []
+    for item in filename:
+        if bool(item) is False:
+            del item
+        else:
+            new_list.append(item)
+    return new_list
+
 
